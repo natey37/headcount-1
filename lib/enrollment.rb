72ef2d5264a8e3de0d81808162b9c6caa96ea061
Nathan Brickett <Nathan Brickett>
2017-05-25 20:09:02 -0400

    enrollment now takes hs grad info

--- lib/enrollment.rb
+++ lib/enrollment.rb
@@ -1,21 +1,35 @@
 
 class Enrollment
-  attr_reader :name, :kindergarten_participation
+  attr_reader :name, :kindergarten_participation, :highschool_graduation
 
   def initialize(hash)
     @name = hash[:name]
     @kindergarten_participation = hash[:kindergarten_participation]
+    @highschool_graduation = hash[:high_school_graduation]
+
+  end
+
+  def graduation_rate_in_year(year)
+    graduation_rate_by_year[year]
+  end
+
+  def graduation_rate_by_year
+    round(highschool_graduation)
   end
 
   def kindergarten_participation_by_year
-    new_hash = {}
-      @kindergarten_participation.map do |k, v|
-        new_hash[k] = v.round(3)
-      end
-        return new_hash
+    round(kindergarten_participation)
   end
 
   def kindergarten_participation_in_year(year)
     kindergarten_participation_by_year[year]
   end
+
+  def round(data)
+    new_hash = {}
+    data.map{|k, v| new_hash[k] = v.round(3)}
+      return new_hash
+  end
+
+
 end
